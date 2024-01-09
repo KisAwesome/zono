01212eb042d8f680ef0ae4d2c9da8fa18d33200a
KisAwesome <cool676rock@gmail.com>
2024-01-09 15:05:53 +0400

    Updated TransmissionError str

--- socket/exceptions.py
+++ socket/exceptions.py
@@ -1,14 +1,14 @@
 errors = {
-    1: "Error occured while recieving data length",
+    1: "Error occurred while receiving data length",
     2: "Operation timed out",
     3: "Data decryption error",
     4: "Bad file descriptor",
     5: "Connection has been reset by the peer",
     6: "Attempting to send through a closed connection",
     7: "Unable to connect to the server",
-    8: "Error occured while exchanging secure keys with the server",
+    8: "Error occurred while exchanging secure keys with the server",
     9: "Initial handshake with the server failed",
-    10: "Lost connection to the server while performing intial handshake",
+    10: "Lost connection to the server while performing initial handshake",
     11: "An unknown error occurred while performing initial handshake",
 }
 
@@ -21,7 +21,7 @@ class TransmissionError(Exception):
         super().__init__()
 
     def __str__(self):
-        return f"Error {self.errorno} - {self.errormsg}"
+        return f"[Errno {self.errorno}] {self.errormsg}"
 
 
 class ReceiveError(TransmissionError):
