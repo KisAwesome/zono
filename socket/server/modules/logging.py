a651248b9cc3e23e2eee0bebcab061b6acfe8f90
KisAwesome <cool676rock@gmail.com>
2024-05-24 21:39:16 +0400

    Added server shutdown logging

--- socket/server/modules/logging.py
+++ socket/server/modules/logging.py
@@ -6,26 +6,28 @@ import yaml
 
 logger = logging.getLogger("zono.server")
 
+
 class Logging(ServerModule):
-    def __init__(self,requests=False):
+    def __init__(self, requests=False, show_event_loggers=False):
+        self.show_event_loggers = show_event_loggers
         self.requests = requests
 
     def setup(self, ctx):
         self.form_addr = ctx.app.form_addr
-        
+
     def module(self, module):
         if self.requests is False:
-            module['events'].pop('on_request_processed')
-            module['events'].pop('after_packet')
+            module["events"].pop("on_request_processed")
+            module["events"].pop("after_packet")
 
-    def calculate_send_length(self,pkts):
+    def calculate_send_length(self, pkts):
         length = 0
         for pkt in pkts:
-            length+= len(yaml.safe_dump(pkt))
+            length += len(yaml.safe_dump(pkt))
         return self.format_file_size(length)
-    
-    def format_file_size(self,size_in_bytes):
-        units = ['B', 'KB', 'MB', 'GB']
+
+    def format_file_size(self, size_in_bytes):
+        units = ["B", "KB", "MB", "GB"]
         threshold = 1024
 
         size = size_in_bytes
@@ -36,14 +38,15 @@ class Logging(ServerModule):
         if int(size) == size:
             size = int(size)
         else:
-            size = round(size,2)
+            size = round(size, 2)
         formatted_size = f"{size}{units[unit_index]}"
         return formatted_size
-
 
     @event()
     def on_connect(self, ctx):
         if ctx.app.is_event_socket(ctx.addr):
+            if self.show_event_loggers is False:
+                return
             return logger.info(
                 f"{self.form_addr(ctx.session.parent_addr)} event socket connected"
             )
@@ -54,6 +57,8 @@ class Logging(ServerModule):
         if (ctx.session is not None) and ctx.session.get(
             "parent_addr", None
         ) is not None:
+            if self.show_event_loggers is False:
+                return
             return logger.info(
                 f"{self.form_addr(ctx.session.parent_addr)} event socket disconnected"
             )
@@ -66,7 +71,9 @@ class Logging(ServerModule):
 
     @event()
     def on_client_error(self, ctx):
-        logger.error(f"Client error {self.form_addr(ctx.addr)}: {ctx.wrapped_error} {ctx._dict.get('pkt','')}")
+        logger.error(
+            f"Client error {self.form_addr(ctx.addr)}: {ctx.wrapped_error} {ctx._dict.get('pkt','')}"
+        )
 
     @event()
     def on_connection_refusal(self, ctx):
@@ -75,14 +82,24 @@ class Logging(ServerModule):
     @event()
     def on_connect_fail(self, ctx):
         logger.error(f"{self.form_addr(ctx.addr)} connection initiation failed")
+
+    @event()
+    def after_packet(self, ctx):
+        ctx.pkt_time = time.time()
+
+    @event()
+    def on_request_processed(self, ctx):
+        elapsed_time = round((time.time() - ctx.pkt_time) * 1000, 3)
+        size = self.calculate_send_length(ctx.responses)
+        recv_s = self.format_file_size(len(yaml.safe_dump(ctx.raw_pkt)))
+        logger.info(
+            f'{ctx.app.form_addr(ctx.addr)} /{ctx.pkt.get("path")} {elapsed_time}ms received {recv_s} sent {size}'
+        )
+
+    @event()
+    def shutdown_server(self,ctx):
+        logger.debug('Shutting down server')
     
     @event()
-    def after_packet(self,ctx):
-        ctx.pkt_time = time.time()
-        
-    @event()
-    def on_request_processed(self,ctx):
-        elapsed_time = round((time.time() - ctx.pkt_time) *1000,3)
-        size =self.calculate_send_length(ctx.responses)
-        recv_s = self.format_file_size(len(yaml.safe_dump(ctx.raw_pkt)))
-        logger.info(f'{ctx.app.form_addr(ctx.addr)} /{ctx.pkt.get("path")} {elapsed_time}ms received {recv_s} sent {size}')
+    def server_closed(self,ctx):
+        logger.important_log('Server closed')
