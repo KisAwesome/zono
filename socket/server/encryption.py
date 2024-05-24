c216847a0f3d1faa087628c36c16c48d5679d850
KisAwesome <cool676rock@gmail.com>
2024-05-24 21:37:16 +0400

    Moved encryption libraries to seperate file

---
+++ socket/server/encryption.py
@@ -0,0 +1,4 @@
+import cryptography.hazmat.primitives.asymmetric.padding
+import cryptography.hazmat.primitives.asymmetric.rsa
+import cryptography.hazmat.primitives.serialization
+import zono.zonocrypt
